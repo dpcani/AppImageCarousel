31a4bfeeb262efbbfbca8a0f20be5a345aff6adb
David de la Puente <david.puente@marbleinteractive.com>
2023-02-10 07:20:56 +0100

    Create a horizontal scrolling images

--- SwiftUICarousel/View/ContentView.swift
+++ SwiftUICarousel/View/ContentView.swift
@@ -8,14 +8,25 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var isCarTapped = false
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        GeometryReader { outerview in
+            ScrollView(.horizontal, showsIndicators: false) {
+                HStack(alignment: .center) {
+                    ForEach(sampleTrips.indices, id: \.self) { index in
+                        GeometryReader { innerView in
+                            TripCardView(destination: sampleTrips[index].destination, imageName: sampleTrips[index].image, isShowDetails: $isCarTapped)
+                            
+                        }
+                        .padding(.horizontal, 20)
+                        .frame(width: outerview.size.width, height: 450)
+                    }
+                }
+            }
+            
         }
-        .padding()
     }
 }
 
