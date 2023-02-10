48dfe8de5d659927501a0782ae872de4c2ef8ae9
David de la Puente <david.puente@marbleinteractive.com>
2023-02-10 07:34:54 +0100

    Prepare the cards without ScrollView

--- SwiftUICarousel/View/ContentView.swift
+++ SwiftUICarousel/View/ContentView.swift
@@ -10,22 +10,21 @@ import SwiftUI
 struct ContentView: View {
     
     @State private var isCarTapped = false
+    @State private var currentTripIndex = 2
     
     var body: some View {
-        GeometryReader { outerview in
-            ScrollView(.horizontal, showsIndicators: false) {
-                HStack(alignment: .center) {
-                    ForEach(sampleTrips.indices, id: \.self) { index in
-                        GeometryReader { innerView in
-                            TripCardView(destination: sampleTrips[index].destination, imageName: sampleTrips[index].image, isShowDetails: $isCarTapped)
-                            
-                        }
-                        .padding(.horizontal, 20)
-                        .frame(width: outerview.size.width, height: 450)
+        GeometryReader { outerView in
+            HStack(alignment: .center, spacing: 0) {
+                ForEach(sampleTrips.indices, id: \.self) { index in
+                    GeometryReader { innerView in
+                        TripCardView(destination: sampleTrips[index].destination, imageName: sampleTrips[index].image, isShowDetails: $isCarTapped)
                     }
+                    .padding(.horizontal, isCarTapped ? 0 : 20)
+                    .frame(width: outerView.size.width, height: 500)
                 }
             }
-            
+            .frame(width: outerView.size.width, height: outerView.size.height, alignment: .leading)
+            .offset(x: -CGFloat(currentTripIndex) * outerView.size.width)
         }
     }
 }
